bfb21210dbd2158f4d156870d502b00651408b8c
Furkan Vural <furkan.vural@hotmail.de>
2024-11-23 23:54:24 +0100

    changes

--- src/components/center/helperClass/applyCurrentPrayerStyles.tsx
+++ src/components/center/helperClass/applyCurrentPrayerStyles.tsx
@@ -13,9 +13,9 @@ export const applyCurrentPrayerStyles = (isCurrent: boolean): StyleResult => {
             containerClassName: 'bg-white bg-opacity-90 border-white',
             containerStyle: { backgroundColor: 'rgba(255, 255, 255, 0.9)' },
             textClassName: 'text-green-600',
-            textStyle: { color: '#004d00' },
+            textStyle: { color: '#003300' },
             timeClassName: 'text-green-600',
-            timeStyle: { color: '#004d00' },
+            timeStyle: { color: '#003300' },
         };
     }
 
