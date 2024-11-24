e1d8d00d5d882dcfd3bee502b01c9857c1c126cf
Furkan Vural <furkan.vural@hotmail.de>
2024-11-24 20:13:11 +0100

    header text white

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ function Header() {
             </div>
             {/* Überschrift */}
             <div className="flex-1 flex items-center justify-center border border-white border-7 rounded-header mr-8 ml-72">
-                <h1 className="text-header px-20 py-4 rounded font-bebas font-bold tracking-wide text-center letter-spacing">
+                <h1 className="text-white text-header px-20 py-4 rounded font-bebas font-bold tracking-wide text-center letter-spacing">
                     IGMG HANNOVER ŞUBESİ AYASOFYA CAMİ
                 </h1>
             </div>
