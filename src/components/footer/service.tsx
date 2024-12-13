5c32990a5bfbd126f7682d67b4253359e3006507
Furkan Vural <furkan.vural@hotmail.de>
2024-12-13 09:45:05 +0100

    changes

--- src/components/footer/service.tsx
+++ src/components/footer/service.tsx
@@ -15,30 +15,3 @@ export const fetchDailyContent = async (): Promise<DailyContentApiResponse> => {
         throw error;
     }
 };
-
-/*
-import { ApiResponse } from "./types";
-
-export const fetchDailyContent = async (): Promise<ApiResponse> => {
-    // Dummy-Daten, die der API-Antwort entsprechen
-    const dummyData: ApiResponse = {
-        data: {
-            id: 0,
-            dayOfYear: 209,
-            verse: "\"Göğün, erimiş maden gibi ve dağların atılmış renkli yün gibi olacağı günü hatırla.(O gün) hiçbir samimi dost, dostunu sormaz.\"",
-            verseSource: "(Meâric, 70/8-10)",
-            hadith: "\"Ramazandan sonra oruçların en faziletlisi, Allah'ın ayı olan Muharrem ayında tutulan oruçtur. Farz namazlardan sonra en faziletli namaz da gece kılınan namzdır.\"",
-            hadithSource: "(Müslim, \"Sıyâm\", 202)",
-            pray: "\"Ey görünen ve görünmeyeni bilen, gökleri ve yeri yaratan, her şeyin Rabbi ve sahibi olan Allah’ım! Ben tanıklık ederim ki Senden başka ilâh yoktur. \nNefsimin şerrinden, şeytanın ve ortaklarının şerrinden sana sığınırım.\"",
-            praySource: "(İbn Hıbbân, \"Ed’ıye\", No: 962; İbn Ebî Şeybe, \"Dua\", 22, No: 29265)"
-        },
-        success: true,
-        message: null
-    };
-
-    // Simuliere eine kurze Verzögerung, um das Laden von Daten zu imitieren
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(dummyData), 500);
-    });
-};
-*/
