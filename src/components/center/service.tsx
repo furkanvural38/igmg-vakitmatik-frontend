ba7b2982d98eff7e6be6ecda9b0a974b0a5d7cdd
Furkan Vural <furkan.vural@hotmail.de>
2025-05-19 21:56:55 +0200

    redesign

--- src/components/center/service.tsx
+++ src/components/center/service.tsx
@@ -2,5 +2,22 @@ import { PrayerTimesApiResponse } from "./types";
 import { fetchPrayerTimesFromLocalExcel } from "../../excelReader/service.ts"; // Lokale Excel-Funktion
 
 export const fetchDailyPrayerTimeWithFallback = async (): Promise<PrayerTimesApiResponse> => {
-    return fetchPrayerTimesFromLocalExcel();
+    try {
+        const response = await fetch("https://igmg-namaz.synology.me:3838/hannover");
+
+        if (!response.ok) {
+            throw new Error("Netzwerkantwort war nicht ok.");
+        }
+
+        const data: PrayerTimesApiResponse = await response.json();
+
+        if (!data.success || !data.data.length) {
+            throw new Error("Unerwartete API-Antwort.");
+        }
+
+        return data;
+    } catch (error) {
+        console.warn("⚠️ Online-Daten nicht verfügbar. Verwende lokale Excel-Datei.");
+        return fetchPrayerTimesFromLocalExcel();
+    }
 };
